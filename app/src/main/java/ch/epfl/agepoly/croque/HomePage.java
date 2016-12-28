d602b8bf7f56450d9c0eeca34d990ecce138472b
Alexis Dewaele <alexis.dewaele@me.com>
2016-12-28 13:17:16 +0100

    Adds Toolbar. Still needs to change its style.

--- app/src/main/java/ch/epfl/agepoly/croque/HomePage.java
+++ app/src/main/java/ch/epfl/agepoly/croque/HomePage.java
@@ -2,6 +2,9 @@ package ch.epfl.agepoly.croque;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Window;
+import android.widget.TextView;
 
 public class HomePage extends AppCompatActivity {
 
@@ -9,5 +12,8 @@ public class HomePage extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_page);
-    }
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setTitle("DEJEUNER");
+   }
 }
