7e68d6373023915356b0f87a33dfc5ee55c1e439
Alexis Dewaele <alexis.dewaele@me.com>
2016-12-29 13:57:56 +0100

    Toolbar show app's title

--- app/src/main/java/ch/epfl/agepoly/croque/HomePage.java
+++ app/src/main/java/ch/epfl/agepoly/croque/HomePage.java
@@ -3,6 +3,7 @@ package ch.epfl.agepoly.croque;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.Window;
 import android.widget.TextView;
 
@@ -11,8 +12,8 @@ public class HomePage extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home_page);
         Toolbar myToolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(myToolbar);
-        setContentView(R.layout.activity_home_page);
     }
 }
