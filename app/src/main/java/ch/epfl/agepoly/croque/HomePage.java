da35c5d4ede5bc42a1db9b6eab77b82e3f8bca58
Alexis Dewaele <alexis.dewaele@me.com>
2016-12-29 14:35:13 +0100

    Adds a settings icon in toolbar

--- app/src/main/java/ch/epfl/agepoly/croque/HomePage.java
+++ app/src/main/java/ch/epfl/agepoly/croque/HomePage.java
@@ -5,6 +5,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.Window;
 import android.widget.TextView;
 
@@ -15,8 +17,16 @@ public class HomePage extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_page);
         Toolbar myToolbar = (Toolbar) findViewById(R.id.toolbar);
-        myToolbar.setTitle(R.string.croque_all_caps);
-        myToolbar.setTitleTextColor(Color.parseColor("#BF3048"));
         setSupportActionBar(myToolbar);
+        //myToolbar.setTitle(R.string.croque_all_caps);
+        myToolbar.setTitleTextColor(Color.parseColor("#BF3048"));
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.options, menu);
+        return true;
     }
 }
